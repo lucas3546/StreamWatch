29f0a57b90bb13992cc82a9251e6a2ac7dbeb721
Lucas <lc45648siete@gmail.com>
2026-01-26 00:11:35 -0300

    production url

--- src/frontend/src/utils/config.ts
+++ src/frontend/src/utils/config.ts
@@ -1,5 +1,5 @@
 //export const BASE_URL = window.location.origin + API_PATH + "/v1";
-export const BASE_ORIGIN_URL = "http://localhost:5100"; // put /api in production //production localhost: https://localhost/api  //development: http://localhost:5100
+export const BASE_ORIGIN_URL = "/api"; // put /api in production //production localhost: https://localhost/api  //development: http://localhost:5100
 export const BASE_URL = BASE_ORIGIN_URL + "/v1";
 
 
